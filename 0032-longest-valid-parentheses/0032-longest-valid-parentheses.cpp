cd8c03bb018116b8ba529804d81ee58d1ac1f6b0
Harrsshhh08 <harsh810xyz@gmail.com>
2026-09-13 21:27:43 +0530

    Time: 0 ms (100%), Space: 11.7 MB (50.23%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         stack<int> st;
-        int top=-1, longest=0;
+        int longest=0;
         st.push(-1);
         for(int i=0;i<s.length();i++){
             if(s[i]=='('){
